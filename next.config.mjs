b74c907609e59d78cb462831888578538bb0910d
liyunfu1998 <liyunfu1998@qq.com>
2024-09-01 22:39:13 +0800

    feat: 测试适配stripe

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import autoCert from "anchor-pki/auto-cert/integrations/next";
+
+const withAutoCert = autoCert({
+  enabledEnv: "development",
+});
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
  
-export default withNextIntl(nextConfig);
+export default withAutoCert(withNextIntl(nextConfig))
