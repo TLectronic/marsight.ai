5ae99288904f29993c8e31f001fc35b4380175b2
tjw <114938661+tjw2003@users.noreply.github.com>
2024-09-07 14:52:49 +0800

    指标加粗，text改为灰色

--- components/ui/databox.tsx
+++ components/ui/databox.tsx
@@ -9,13 +9,12 @@ interface DataBoxProps extends React.HTMLAttributes<HTMLDivElement> {
 const DataBox = React.forwardRef<HTMLDivElement, DataBoxProps>(
   ({ spanText, paragraphText, icon, className, ...props }, ref) => {
     return (
-      <div ref={ref} className={`custom-component ${className}`} {...props} >
-        <p className="text-2xl mb-2">{paragraphText}</p>
-        <div className="text-xs">
-          <span className="flex items-center">
-            {icon && <span className="mr-1">{icon}</span>}
-            {spanText}
-          </span>
+      <div ref={ref} className={`custom-component ${className}`} {...props}>
+        <p className="text-2xl font-bold mb-2">{paragraphText}</p>
+
+        <div className="text-xs flex items-center text-gray-500">
+          {icon && <span className="mr-1">{icon}</span>}
+          <span>{spanText}</span>
         </div>
       </div>
     );
