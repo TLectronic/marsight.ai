70b000df21c9153d0cffc6b474d267f96ccc9c7e
TLectronic <3051732820@qq.com>
2024-09-08 19:52:49 +0800

    feat：优化stripe问题

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,20 +1,20 @@
 "use client"
-import {useTranslations} from 'next-intl';
-import {Link} from '@/i18n/routing';
+import { useTranslations } from 'next-intl';
+import { Link } from '@/i18n/routing';
 import { redirect } from 'next/navigation';
- 
-import CheckoutPage from '@/components/CheckoutPage';
-import convertToSubcurrency from '@/lib/convertToSubcurrency'; 
-import {Elements} from '@stripe/react-stripe-js'
-import {loadStripe} from '@stripe/stripe-js'
 
-if(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
+import CheckoutPage from '@/components/CheckoutPage';
+import convertToSubcurrency from '@/lib/convertToSubcurrency';
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
   throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined')
 }
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
-export default function HomePage({params: {locale}}: {params: {locale: string}}) {
-  // redirect(`/${locale}/search`)
+export default function HomePage({ params: { locale } }: { params: { locale: string } }) {
+  redirect(`/${locale}/search`)
   const t = useTranslations('HomePage');
   const amount = 49.99;
 
